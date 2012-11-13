5e74e130c82803426716c182d0c7ef151f9a8d0f
elico <elico_yqf@sina.com>
2012-11-13 17:11:03 +0800

    添加了后台制做基本命令，还需要进一步测试。

--- app/helpers/business_logic_helper.rb
+++ app/helpers/business_logic_helper.rb
@@ -1,5 +1,7 @@
+require 'net/telnet'
+
 module BusinessLogicHelper
-    #分割数据并且将数据插入到数据库中
+  #分割数据并且将数据插入到数据库中
   def split_data(content)
     linenu = content.to_s.split(/\n/)
 
@@ -19,6 +21,42 @@ module BusinessLogicHelper
       linefh.delete_if { |x| x.empty? }
       puts "linffh is :#{linefh}"
       linefh
+    end
+  end
+
+  def make_data()
+
+    telnet = Net::Telnet.new(
+        "Host"     => '192.166.16.3',
+        "Port"     => 10001,
+        "Timeout"  => 10,
+        "Waittime" => 5
+    )
+    begin
+
+      telnet.waitfor(/>/) { |c| print c }
+      telnet.puts "MM"
+      puts "<----------------------------->1"
+
+      telnet.waitfor(/USERID:/) { |c| print c }
+      telnet.puts "PW0009"
+
+      puts "<----------------------------->2"
+      telnet.waitfor(/PASSWORD:/) { |c| print c }
+      telnet.puts "PW0009"
+      puts "<----------------------------->3"
+
+      telnet.puts "4294:dn=k'6994951,subgrp=5."
+      telnet.waitfor(/>/) { |c| print c }
+    rescue
+      #为了防止未超时的会话，直接输入命令即可。
+      telnet.puts "4294:dn=k'6994951,subgrp=5."
+      telnet.waitfor(/>/) { |c| print c }
+      puts "<----------------------------->4"
+    ensure
+
+      telnet.close
+
     end
   end
 
