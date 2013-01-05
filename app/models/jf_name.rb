2976715803b57e43a8dfb8e335bfdb58633fe0f8
elico <elico_yqf@sina.com>
2013-01-05 17:16:42 +0800

    交班日志完善v0.6.1（修正了一些在部署方面的小bug）

--- app/models/jf_name.rb
+++ app/models/jf_name.rb
@@ -1,4 +1,4 @@
 class JfName < ActiveRecord::Base
-  attr_accessible :ip_address, :name ,:status
+  attr_accessible :ip_address, :name, :status, :p_emerg, :p_int, :p_local, :p_nat, :p_suburban
   has_many :dn_tables
 end
