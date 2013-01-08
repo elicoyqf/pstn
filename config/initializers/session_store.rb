fcec56fe06b31896e8cc24490c1c29b5e40212a4
elico <elico_yqf@sina.com>
2013-01-08 14:52:34 +0800

    在session_store里面添加了session的过期时间为1个小时。

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Pstn::Application.config.session_store :cookie_store, key: '_pstn_session'
+Pstn::Application.config.session_store :cookie_store, key: '_pstn_session', expire_after: 60.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
