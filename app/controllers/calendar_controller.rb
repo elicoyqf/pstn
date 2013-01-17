17ca1cc74e65dca76bc9413d234d18c5134c17ed
elico <elico_yqf@sina.com>
2013-01-18 01:19:16 +0800

    (添加注释)添加了多用户交班日志相互屏蔽功能。

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -8,6 +8,7 @@ class CalendarController < ApplicationController
 
     @event_strips = Event.event_strips_for_month(@shown_month)
 
+    #此处是为了屏蔽多部门用户相互交叉的交班日志，实际底层是在一起的，显示的时候进行过滤。
     @event_strips.each do |x|
       x.each_with_index do |y,i|
         unless y.blank?
