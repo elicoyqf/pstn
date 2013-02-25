7e2ed7b5b157284b273e95451e09500a9e4e47e1
elico <elico_yqf@sina.com>
2013-02-25 11:30:48 +0800

    修正了r_cmd不为空导致无法发送cmd的问题，软件版更新至v2.2.22

--- lib/share/work_order_process.rb
+++ lib/share/work_order_process.rb
@@ -261,7 +261,7 @@ module WorkOrderProcess
           puts 'ad_cmd---->'+ad_cmd
           puts 'test_r---->'+test_r.to_s
 
-          #pstn_data(ip_address, x.id, cmd, ad_cmd, cf_no_cmd, cf_act_cmd, df_cmd, r_cmd, check)
+          pstn_data(ip_address, x.id, cmd, ad_cmd, cf_no_cmd, cf_act_cmd, df_cmd, r_cmd, check)
         else
           if dn.blank?
             WorkOrder.find(x.id).update_attribute(:status, 4)
